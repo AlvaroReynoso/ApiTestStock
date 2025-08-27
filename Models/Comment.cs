a0ee16d3abbb0c53308c6fdedfc04a5ffa381934
AlvaroReynoso <areynoso@frro.utn.edu.ar>
2025-08-27 02:41:39 -0300

    Add initial project files and dependencies for API
    - Created new cache files for assembly info, MSBuild editor config, and global usings.
    - Added generated files for MVC application parts and assets.
    - Included project.assets.json and project.nuget.cache for package management.
    - Integrated Swashbuckle.AspNetCore and Microsoft.AspNetCore.OpenApi packages for API documentation.
    - Established static web assets configuration for the project.

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+namespace Models;
+
+public class Comment
+{
+    public int id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int? StockId { get; set; }
+    // navigation
+    public Stock? Stock { get; set; }
+}
